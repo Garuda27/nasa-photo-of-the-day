cf3c6dd99338ff1954c19d3fa8504afe5e09775f
Preston <pres.10.nap@gmail.com>
2019-07-17 21:26:07 -0400

    not working, but here's what I got so far

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import Grid from './components/nasa.js';
+import axios from "axios";
 function App() {
+  const [data, setData] = useState([])
+
+  const fetchData = () => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15')
+      .then(response => {
+          setData(response.data)
+      })
+      .catch(error => {console.log('error in fetchData')})
+  }
+
+    useEffect(fetchData, [])
+
+    console.log(data);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      
     </div>
   );
 }
---
+++ src/components/nasa.js
@@ -0,0 +1,11 @@
+import React from "react" 
+
+
+    export default function Posts(props) {
+        return (
+            <div>
+                <h1>{props.title}</h1>
+                <iframe title="video" src={props.nasaVid}></iframe>
+            </div>
+        )
+    } 
