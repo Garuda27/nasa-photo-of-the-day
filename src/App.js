5df866dc6c301375cadf9a5f3bce9a7df212be75
Preston <pres.10.nap@gmail.com>
2019-07-18 20:23:41 -0400

    here's what I got so far, finishing styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import Grid from './components/nasa.js';
 import axios from "axios";
+import styled from "styled-components"; 
+
+const Background = styled.div`
+  background: black;
+`;
+
+const Title = styled.div`
+  color: red;
+`; 
+
+const Date = styled.div`
+  color: red;
+`; 
+
+const Content = styled.div`
+  width: 80%;
+  display: flex;
+  align-items: center;
+  justify-items: center;
+`; 
+
 function App() {
   const [data, setData] = useState([])
 
@@ -10,16 +30,30 @@ function App() {
       .then(response => {
           setData(response.data)
       })
-      .catch(error => {console.log('error in fetchData')})
+      .catch(error => {console.log('error in Data')})
   }
 
     useEffect(fetchData, [])
 
     console.log(data);
-
+    
   return (
     <div className="App">
-      
+      <Background>
+      <Title>
+        <h1>NASA</h1>
+      </Title>
+      <Date>
+        <h2>{data.date}</h2>
+      </Date>
+      </Background>
+        <img src={data.url} alt="ISS in Front of Sun"></img>
+        <Content>
+          <div className="content">
+            <h3>{data.title}</h3>
+              <p>{data.explanation}</p>
+          </div>
+        </Content>
     </div>
   );
 }
